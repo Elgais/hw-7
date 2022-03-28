a261fb953a5ccd21eb4cb19dd8e18b7f8feb4686
Elga <isaevelgayz05@gmail.com>
2022-03-28 23:39:39 +0600

    hw##7

--- Elgazy_16-1_hw#7.py
+++ Elgazy_16-1_hw#7.py
@@ -6,9 +6,9 @@ result = 0
 while (s!=0):
     dig = s%10
     result = result*10 + dig
-    s = int(s/10)
+    s = int(s//10)
 
-if (result == c_s):
-    print('Palindrom! ')
-else:
-    print('NOT PALINDROM!')
+    if (result == c_s):
+        print('Palindrom!' ,result)
+    else:
+        print('NOT PALINDROM!' )
